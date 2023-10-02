fe74e2cfa16667592d29e8be1305e937904001ec
MelisaRivas <rivasmelisa2024@gmail.com>
2023-10-02 14:46:13 -0600

    pre2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import NavBar from './components/NavBar/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { useState } from 'react';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <>
       <NavBar />
       <ItemListContainer greeting='¡Bienvenidos a Koffie!' />
+      <ItemCount initial={1} stock={25} onAdd={(quantity)=>console.log('cantidad agregada', quantity)} />
     </>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartWidget from '../CartWidget/CartWidget';
 import './NavBar.css';
 
@@ -13,14 +12,9 @@ function NavBar(){
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="#home">Inicio</Nav.Link>
-                <Nav.Link href="#link">Tienda</Nav.Link>
-                <NavDropdown title="Sobre Nosotros" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.2"> Cultivo del café</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Cómo preparamos el café</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4"> Contacto </NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link href="#home">Café helado</Nav.Link>
+                <Nav.Link href="#link">Cafe caliente</Nav.Link>
+                <Nav.Link href="#home">Otros</Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Container>
