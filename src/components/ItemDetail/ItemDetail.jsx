f4b59d254e7582ef14bff8bd54e6f97bdddef577
MelisaRivas <rivasmelisa2024@gmail.com>
2023-10-20 22:19:55 -0600

    empezando pf

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,25 +1,41 @@
 import React from 'react'
 import ItemCount from '../ItemCount/ItemCount'
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const ItemDetail = ({id, nombre, imagen, categoria, precio, tamanio}) => {
-  return (
+  
+    const [quantityAdded, setQuantityAdded] = useState(0)
+
+    const handleOnAdd = (quantity) => {
+        setQuantityAdded(quantity)
+    }
+  
+    return (
     <>
-    <article className='CardItem'>
-        <header className='Header'>
-            <h2 className='ItemHeader'>{id}. {nombre}</h2>
-        </header>
-        <picture>
-            <img src={imagen} alt={nombre} className='ItemImg' />
-        </picture>    
-        <section>
+    <Card className='card col-6 p-center'>
+        <Card.Body>
+        <Card.Title className='tituloCard'>{id}. {nombre}</Card.Title>
+        <picture> <img src={imagen} alt={nombre} className='ItemImg' /> </picture>    
+        <Card.Text>
             <p className='Info'> Categoria: {categoria}</p>
             <p className='Info'> Precio: {precio}</p>
             <p className='Info'> Tamaño: {tamanio}</p>
-        </section>
+        </Card.Text>
         <footer className='ItemFooter'>
-            <ItemCount initial={1} stock={25} onAdd={(quantity)=>console.log('cantidad agregada', quantity)}/>
+            {
+                quantityAdded > 0 ? (
+                    <Link to='/cart' className='Option'> Terminar compra</Link>
+                ) : (
+                    <ItemCount initial={1} stock={35} onAdd={handleOnAdd}/>
+                )
+            }
+
         </footer>
-    </article>
+        </Card.Body>
+    </Card>
     </>  
   )
 }
